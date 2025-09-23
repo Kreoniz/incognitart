f9c87cc94e87ca4b2038eeb8ab95641a7d252f99
Kreoniz <kreoniz@mail.ru>
2025-09-23 21:44:21 +0600

    feat(frontend): connect to backend

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,6 +1,24 @@
 import { NavLink, Outlet } from "react-router";
 
 export function Layout() {
+  async function handleGetText() {
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    const response = await fetch(`${API_URL}`);
+    const responseJson = await response.json();
+
+    const now = new Date();
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    const milliseconds = String(now.getMilliseconds()).padStart(3, "0");
+    const timeString = `${minutes}:${seconds}.${milliseconds}`;
+
+    console.log(
+      `%c${timeString} ${JSON.stringify(responseJson)}`,
+      "font-size: 1rem; color: coral;",
+    );
+  }
+
   return (
     <div>
       <nav className="flex gap-2">
@@ -12,6 +30,10 @@ export function Layout() {
           Gallery
         </NavLink>
       </nav>
+
+      <button onClick={handleGetText} className="rounded p-2 outline">
+        console log response
+      </button>
 
       <Outlet />
     </div>
